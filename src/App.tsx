321f2d012a6c1ee4651a7bc220c548d4c3cf875a
Natália <natalia.setubal@outlook.com>
2021-08-05 14:33:52 -0300

    autenticação com o firebase, utilizando a conta do google

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route} from 'react-router-dom'
+
 import { Button } from "./components/Button";
 import { Home } from "./pages/Home";
+import {NewRoom} from './pages/NewRoom'
 
 function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+      <Route path='/' exact component={Home}></Route>
+      <Route path='/rooms/new' component={NewRoom}></Route>
+    </BrowserRouter>
   );
 }
 
